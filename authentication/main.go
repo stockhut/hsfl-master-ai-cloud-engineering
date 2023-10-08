5f6c6674b0593d0b8cd633ee2a98b331f6586ff9
neki9072 <kirchner.nele@outlook.de>
2023-10-08 18:27:04 +0200

    feat(authentication): temp database and handle login

--- authentication/main.go
+++ authentication/main.go
@@ -7,12 +7,26 @@ import (
 	"github.com/stockhut/hsfl-master-ai-cloud-engineering/common/router"
 )
 
+type account struct {
+	name     string
+	email    string
+	password string
+}
+
+var accounts []account = make([]account, 0) //temporary database
+
 func main() {
+	accounts = append(accounts, account{name: "Nele", email: "nele@nele.de", password: "xyz123"})
+	accounts = append(accounts, account{name: "Alex", email: "alex@nele.de", password: "abc123"})
+	accounts = append(accounts, account{name: "Fabi", email: "fabi@nele.de", password: "def123"})
+
 	fmt.Println("Hello from Auth!")
 
 	r := router.New()
 
 	r.GET("/", handleHi)
+	r.POST("/createAccount", handleCreateAccount)
+	r.GET("/login/:name/:password", handleLogin)
 
 	err := http.ListenAndServe("localhost:8080", r)
 	panic(err)
@@ -20,4 +34,25 @@ func main() {
 
 func handleHi(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hi!")
+}
+
+func handleCreateAccount(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Account Created!")
+
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	for _, acc := range accounts {
+		if r.Context().Value("name") == acc.name {
+			if r.Context().Value("password") == acc.password {
+				fmt.Fprintln(w, "Login")
+			} else {
+				fmt.Fprintln(w, "Falsches Passwort!")
+
+			}
+			return
+		}
+
+	}
+	w.WriteHeader(http.StatusNotFound)
 }
