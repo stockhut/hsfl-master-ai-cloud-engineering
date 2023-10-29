85126e09e601e7786e65c739ee56b739c846d1b2
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-10-29 17:05:07 +0100

    test(common): test request logging middleware

--- common/middleware/request-logger/request_logging_test.go
+++ common/middleware/request-logger/request_logging_test.go
@@ -1,7 +1,9 @@
 package request_logger
 
 import (
+	"bytes"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -25,5 +27,38 @@ func Test_responseWriterWithStatus_WriteHeader(t *testing.T) {
 
 		assert.Equal(t, http.StatusTeapot, *responseWriter.statusCode)
 		assert.Equal(t, recorder.Result().StatusCode, *responseWriter.statusCode)
+	})
+}
+
+func Test_request_logger_middleware(t *testing.T) {
+
+	t.Run("logs someething", func(t *testing.T) {
+
+		recorder := httptest.NewRecorder()
+
+		// record bytes written by the middleware
+		logData := make([]byte, 0)
+		logWriter := bytes.NewBuffer(logData)
+
+		logger := log.New(logWriter, "", 0)
+
+		handlerCalled := false
+
+		mw := New(logger)
+
+		r := httptest.NewRequest(http.MethodPost, "/test", nil)
+		handler := mw(func(w http.ResponseWriter, r *http.Request) {
+			handlerCalled = true
+			w.WriteHeader(http.StatusTeapot)
+		})
+
+		handler(recorder, r)
+
+		assert.NotEmpty(t, true, logData)
+		assert.Equal(t, true, handlerCalled)
+
+		logString := logWriter.String()
+		assert.Contains(t, logString, "POST")
+		assert.Contains(t, logString, "418")
 	})
 }
