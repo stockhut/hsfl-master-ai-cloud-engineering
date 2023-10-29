fd96358f3284ef39217859cda4bf3ee51a8929ca
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-10-29 16:42:17 +0100

    feat(common): colorize request logging output

--- common/middleware/request-logger/request_logging.go
+++ common/middleware/request-logger/request_logging.go
@@ -1,8 +1,11 @@
 package request_logger
 
 import (
+	tc "github.com/stockhut/hsfl-master-ai-cloud-engineering/common/termcolor"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -41,14 +44,51 @@ func New(logger *log.Logger) func(next http.HandlerFunc) http.HandlerFunc {
 
 			next(&responseWriter, r)
 
-			method := r.Method
+			method := httpMethodColorizer(r.Method)
 			url := r.URL
 			duration := time.Now().Sub(startTime)
-			statusCode := responseWriter.statusCode
+			var statusCode string
+			if logger.Writer() == os.Stdout {
+				statusCode = statusCodeColorizer(*responseWriter.statusCode)
+			} else {
+				statusCode = strconv.Itoa(*responseWriter.statusCode)
+			}
 
 			// [GET] /foo/bar 404 12ms
-			logger.Printf("[%s] %s %d %s", method, url, *statusCode, duration)
-
+			logger.Printf("[%s] %s %s %s", method, url, statusCode, duration)
 		}
 	}
+}
+
+func statusCodeColorizer(code int) string {
+	return tc.Bg(httpStatusColor(code))(strconv.Itoa(code))
+}
+
+func httpStatusColor(code int) tc.BgColor {
+
+	switch {
+	case code < 200:
+		return tc.BgBrightBlue // 100-199
+	case code < 300:
+		return tc.BgGreen // 200-299
+	case code < 400:
+		return tc.BgCyan // 300-299
+	case code < 500:
+		return tc.BgRed // 400-499
+	case code < 600:
+		return tc.BgRed // 500-599
+	default:
+		return tc.BgUndefined
+	}
+}
+
+func httpMethodColorizer(method string) string {
+	colors := map[string]tc.BgColor{
+		http.MethodGet:    tc.BgGreen,
+		http.MethodPost:   tc.BgBlue,
+		http.MethodPut:    tc.BgCyan,
+		http.MethodDelete: tc.BgRed,
+	}
+
+	return tc.Bg(colors[method])(method)
 }
