d74b8ba6c46270c641539e7f2982c560f0a75381
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-11-03 13:20:25 +0100

    feat(web-service): init static web server

---
+++ web-service/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	fmt.Println("Hello from the static file server")
+
+	router := http.NewServeMux()
+	router.Handle("/static/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:3000", router))
+}
