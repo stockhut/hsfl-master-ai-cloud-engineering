b1a023741bc8b1656696af5aa3dcffc79c0e1a0a
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-11-15 20:37:53 +0100

    test(recipe): test health endpoint

---
+++ recipe/recipes/health_test.go
@@ -0,0 +1,19 @@
+package recipes
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestController_GetHealth(t *testing.T) {
+
+	recorder := httptest.NewRecorder()
+
+	controller := NewController(nil)
+
+	controller.GetHealth(recorder, nil)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+}
