d02a8189ef7f20cd4a2578fe94b5a2f8d49211c0
neki9072 <68080891+neki9072@users.noreply.github.com>
2023-12-09 08:48:44 +0100

    test(authentication): controller test

--- authentication/accounts/controller_test.go
+++ authentication/accounts/controller_test.go
@@ -13,7 +13,6 @@ import (
 	"github.com/stockhut/hsfl-master-ai-cloud-engineering/authentication/accounts/model"
 	"github.com/stockhut/hsfl-master-ai-cloud-engineering/authentication/accounts/repository"
 	"github.com/stockhut/hsfl-master-ai-cloud-engineering/authentication/jwt_util"
-	"github.com/stockhut/hsfl-master-ai-cloud-engineering/common/router"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 )
@@ -22,25 +21,18 @@ func TestAccountController(t *testing.T) {
 	privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	tokenGenerator := jwt_util.NewJwtTokenGeneratorWithKey(privateKey)
 
-	gomockController := gomock.NewController(t)
-
-	mockRepo := mock_accounts.NewMockAccountRepository(gomockController)
-	//mockRepo.EXPECT().CreateAccount(gomock.Any()).Return(nil).Times(1)
-	//mockRepo.EXPECT().CheckDuplicate(gomock.Any()).Return(repository.NO_DUPLICATES, nil).Times(1)
-
-	//FindAccount(name string) (*model.Account, error)
-	c := Controller{accountRepo: mockRepo, tokenGenerator: *tokenGenerator}
-
-	router := router.New()
-	router.POST("/account", c.HandleCreateAccount)
-	router.POST("/login", c.HandleLogin)
-
 	t.Run("CreateAccount", func(t *testing.T) {
 		t.Run("should return 400 BAD REQUEST if payload is nil", func(t *testing.T) {
+			gomockController := gomock.NewController(t)
+
+			mockRepo := mock_accounts.NewMockAccountRepository(gomockController)
+
+			c := Controller{accountRepo: mockRepo, tokenGenerator: *tokenGenerator}
 			w := httptest.NewRecorder()
 			r := httptest.NewRequest(http.MethodPost, "/account", nil)
 
-			router.ServeHTTP(w, r)
+			c.HandleCreateAccount(w, r)
+
 			assert.Equal(t, http.StatusBadRequest, w.Code)
 		})
 		//AB HIER no tests to run
@@ -79,55 +71,80 @@ func TestAccountController(t *testing.T) {
 
 			c.HandleCreateAccount(w, r)
 
-			router.ServeHTTP(w, r)
 			assert.Equal(t, http.StatusCreated, w.Code)
 		})
 	})
 
 	t.Run("LoginAccount", func(t *testing.T) {
 		t.Run("should return 400 BAD REQUEST if payload is nil", func(t *testing.T) {
+			gomockController := gomock.NewController(t)
+
+			mockRepo := mock_accounts.NewMockAccountRepository(gomockController)
+
+			c := Controller{accountRepo: mockRepo, tokenGenerator: *tokenGenerator}
 			w := httptest.NewRecorder()
 			r := httptest.NewRequest(http.MethodPost, "/login", nil)
 
-			router.ServeHTTP(w, r)
+			c.HandleLogin(w, r)
 			assert.Equal(t, http.StatusBadRequest, w.Code)
 
 			assert.Nil(t, FindCookie(w.Result().Cookies(), "jwt"), "did not expect jwt token as cookie")
 		})
 
 		t.Run("should return 400 BAD REQUEST if password is wrong", func(t *testing.T) {
-			testBody := `{"name":"Nele", "password":"wrong"}`
+			gomockController := gomock.NewController(t)
+			modelAccount := model.Account{
+				Name:     "Nele",
+				Email:    "nele@nele.de",
+				Password: "1234",
+			}
 
+			mockRepo := mock_accounts.NewMockAccountRepository(gomockController)
+			mockRepo.EXPECT().FindAccount(modelAccount.Name).Return(&modelAccount, nil).Times(1)
+
+			testBody := `{"name":"Nele", "password":"wrong"}`
+			c := Controller{accountRepo: mockRepo, tokenGenerator: *tokenGenerator}
 			w := httptest.NewRecorder()
 			r := httptest.NewRequest(http.MethodPost, "/login", strings.NewReader(testBody))
 
-			router.ServeHTTP(w, r)
+			c.HandleLogin(w, r)
 			assert.Equal(t, http.StatusNotFound, w.Code)
-
 			assert.Nil(t, FindCookie(w.Result().Cookies(), "jwt"), "did not expect jwt token as cookie")
 		})
 
 		t.Run("should return 400 BAD REQUEST if username does not exist", func(t *testing.T) {
-			testBody := `{"name":"doesnotexist", "password":"xyz123"}`
+			gomockController := gomock.NewController(t)
+			mockRepo := mock_accounts.NewMockAccountRepository(gomockController)
+			mockRepo.EXPECT().FindAccount("doesnotexist").Return(nil, nil).Times(1)
 
+			testBody := `{"name":"doesnotexist", "password":"xyz123"}`
+			c := Controller{accountRepo: mockRepo, tokenGenerator: *tokenGenerator}
 			w := httptest.NewRecorder()
 			r := httptest.NewRequest(http.MethodPost, "/login", strings.NewReader(testBody))
 
-			router.ServeHTTP(w, r)
+			c.HandleLogin(w, r)
 			assert.Equal(t, http.StatusNotFound, w.Code)
-
 			assert.Nil(t, FindCookie(w.Result().Cookies(), "jwt"), "did not expect jwt token as cookie")
 		})
 
 		t.Run("should return 200 OK if login is successful", func(t *testing.T) {
-			testBody := `{"name":"Nele", "password":"xyz123"}`
+			gomockController := gomock.NewController(t)
+			modelAccount := model.Account{
+				Name:     "Nele",
+				Email:    "nele@nele.de",
+				Password: "1234",
+			}
 
+			mockRepo := mock_accounts.NewMockAccountRepository(gomockController)
+			mockRepo.EXPECT().FindAccount(modelAccount.Name).Return(&modelAccount, nil).Times(1)
+
+			testBody := `{"name":"Nele", "password":"1234"}`
+			c := Controller{accountRepo: mockRepo, tokenGenerator: *tokenGenerator}
 			w := httptest.NewRecorder()
 			r := httptest.NewRequest(http.MethodPost, "/login", strings.NewReader(testBody))
 
-			router.ServeHTTP(w, r)
+			c.HandleLogin(w, r)
 			assert.Equal(t, http.StatusOK, w.Code)
-
 			assert.NotNil(t, FindCookie(w.Result().Cookies(), "jwt"), "expected jwt token as cookie")
 		})
 	})
