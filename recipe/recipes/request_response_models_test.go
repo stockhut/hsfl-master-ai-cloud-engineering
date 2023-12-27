b5c719ae16c0ddf745b69d66bb4845007cc0dbe6
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-12-27 18:35:57 +0100

    feat(recipe): allow more than one ingredient to be added via frontend
    backend: parse ingredient-* fields created by submitted forms

--- recipe/recipes/request_response_models_test.go
+++ recipe/recipes/request_response_models_test.go
@@ -1,6 +1,7 @@
 package recipes
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stockhut/hsfl-master-ai-cloud-engineering/recipe/recipes/model"
@@ -124,4 +125,69 @@ func Test_recipeRequestToModel(t *testing.T) {
 		Difficulty:   "easy",
 		FeedsPeople:  10,
 	}, recipe)
+}
+
+func Test_createRecipeRequestBody_UnmarshalJSON(t *testing.T) {
+
+	t.Run("createRecipeRequestBody UnmarshalJSON", func(t *testing.T) {
+
+		t.Run("unmarshals ingredient attributes when they are string or []string", func(t *testing.T) {
+			testCases := []struct {
+				name                string
+				input               string
+				expectedIngredients []ingredientRequestBody
+			}{
+				{
+					name: "ingredient-* as array",
+					input: `
+						{
+						"ingredient-name":["first","second"],
+						"ingredient-unit":["u1","u2"],
+						"ingredient-amount":["1","2"]
+						}`,
+					expectedIngredients: []ingredientRequestBody{
+						{
+							Name:   "first",
+							Unit:   "u1",
+							Amount: 1,
+						},
+						{
+							Name:   "second",
+							Unit:   "u2",
+							Amount: 2,
+						},
+					},
+				},
+				{
+					name: "ingredient-* as single string",
+					input: `
+						{
+						"ingredient-name":"first",
+						"ingredient-unit":"u1",
+						"ingredient-amount":"1"
+						}`,
+					expectedIngredients: []ingredientRequestBody{
+						{
+							Name:   "first",
+							Unit:   "u1",
+							Amount: 1,
+						},
+					},
+				},
+			}
+
+			for _, tc := range testCases {
+				t.Run(tc.name, func(t *testing.T) {
+					var obj createRecipeRequestBody
+
+					err := json.Unmarshal([]byte(tc.input), &obj)
+
+					assert.Nil(t, err)
+
+					assert.Equal(t, tc.expectedIngredients, obj.Ingredients)
+				})
+			}
+
+		})
+	})
 }
